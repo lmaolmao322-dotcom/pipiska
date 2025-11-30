0f329b4f313771990318407fe3f22ebe88a2d60e
322 <gurusx@yandex.ru>
2025-11-30 15:06:35 +0700

    complete homework2

---
+++ src/main/kotlin/lesson_1/homework2.kt
@@ -0,0 +1,16 @@
+package org.example.lesson_1
+
+fun main() {
+    val numberOfOrders: Int = 75
+    val thanksForOrder: String = "Спасибо за заказ"
+
+    println("Количество заказов: $numberOfOrders\n$thanksForOrder")
+
+    var workers: Int = 2000
+
+    //println(workers)
+
+    workers -= 1
+
+    println(workers)
+}
